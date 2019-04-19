6226587734be736e5d29af51dbf16cfdc4221c40
arielalvesdutra <alves.lsn@gmail.com>
2019-04-19 14:12:50 -0300

    public/index.php: - injeta objeto com a conexão com o banco de dados no $container - injeta a controller TicketWindowController no $conteiner - insere configurações de debug e middleware no $container - injeta o container no $slim - adiciona rota POST - /ticket-window - :create

--- public/index.php
+++ public/index.php
@@ -2,12 +2,73 @@
 
 require '../bootstrap.php';
 
+use App\Controllers\TicketWindowController;
 use Slim\App;
+use Slim\Container;
 
-$slim =  new App();
+/**
+ * Container
+ */
+$container = new Container();
+
+/**
+ * Injeta a conexão com o banco de dados.
+ *
+ * @return \Doctrine\DBAL\Connection
+ */
+$container['Connection'] = function () {
+
+    $config = new \Doctrine\DBAL\Configuration();
+
+    $connectionParameters = [
+        'user' => 'root',
+        'host' => '192.168.11.101',
+        'driver' => 'pdo_mysql',
+        'password' => 'password',
+        'port' => '3601',
+        'dbname' => 'aps'
+    ];
+
+    return \Doctrine\DBAL\DriverManager::getConnection($connectionParameters, $config);
+};
+
+/**
+ * Injeta TicketWindowController
+ * @param $container
+ * @return TicketWindowController
+ */
+$container[TicketWindowController::class] = function ($container)
+{
+    return new \App\Controllers\TicketWindowController(
+        new \App\Services\TicketWindowService(
+            $container['Connection'],
+            new \App\Repositories\TicketWindowRepository($container['Connection'])
+        )
+    );
+};
+
+/**
+ * Define configuração de middleware e de debug
+ */
+$container->get('settings')
+    ->replace([
+        'debug' => true,
+        'determineRouteBeforeAppMiddleware' => true,
+        'displayErrorDetails' => true
+]);
+
+/**
+ * Instancia o Slim
+ */
+$slim = new App($container);
 
 $slim->get('/', function() {
     return "Primeira rota.";
 });
+
+/**
+ * TicketWindow
+ */
+$slim->post('/ticket-window', TicketWindowController::class . ":create");
 
 $slim->run();
