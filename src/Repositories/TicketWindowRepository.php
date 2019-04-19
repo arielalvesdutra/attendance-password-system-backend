b75acdbbc3a733175509255b9b9eda0d6ea81089
arielalvesdutra <alves.lsn@gmail.com>
2019-04-19 14:21:13 -0300

    src/Repositories/TicketWindowRepository.php: - implementa métodos findByName(), find() e findAll() - adiciona nome a tabela de uso da repository - herda a classe AbstractRepository

--- src/Repositories/TicketWindowRepository.php
+++ src/Repositories/TicketWindowRepository.php
@@ -2,24 +2,66 @@
 
 namespace App\Repositories;
 
+use App\Exceptions\NotFoundException;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\FetchMode;
 
-class TicketWindowRepository
+class TicketWindowRepository extends AbstractRepository
 {
+    /**
+     * @var Connection
+     */
     protected $connection;
 
-    public function __construct(Connection $connection)
-    {
-        $this->connection = $connection;
-    }
+    protected $tableName = 'ticket_window';
 
     public function find(int $id)
     {
+        $ticketWindowEntity = $this->connection->createQueryBuilder()
+            ->select()
+            ->from($this->getTableName(), 'tw')
+            ->where('tw.id = ' . $id)
+            ->orderBy('tw.name')
+            ->execute()
+            ->fetch();
 
+        if (empty($ticketWindowEntity)) {
+            throw new NotFoundException('Nenhum registro de guiche encontrado');
+        }
+
+        return $ticketWindowEntity;
     }
 
     public function findAll()
     {
+        $ticketWindowEntities = $this->connection->createQueryBuilder()
+            ->select()
+            ->from($this->getTableName(), 'tw')
+            ->orderBy('tw.name')
+            ->execute()
+            ->fetchAll();
 
+        if (empty($ticketWindowEntities)) {
+            throw new NotFoundException('Nenhum registro de guiche encontrado');
+        }
+
+        return $ticketWindowEntities;
+    }
+
+    public function findByName(string $name)
+    {
+        $ticketEntities = $this->connection->createQueryBuilder()
+            ->select('*')
+            ->from($this->getTableName(), 'tw')
+            ->where( "tw.name = ?")
+            ->setParameter('0', $name)
+            ->execute()
+            ->fetchAll();
+
+        if (empty($ticketEntities)) {
+            throw new NotFoundException('Nenhum registro de guiche encontrado');
+        }
+
+        return $ticketEntities;
     }
 }
